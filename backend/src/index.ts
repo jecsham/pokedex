3c93891548bb704dbd562d373359511eae2e631c
jecsham <jecsham27@gmail.com>
2021-07-10 14:52:34 -0500

    backend: implemented cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import path from "path";
 import express from "express";
+import cors from "cors";
 import { api } from "./api";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(cors());
 app.use(express.static(path.resolve(__dirname, "../build")));
 
 // api routes
