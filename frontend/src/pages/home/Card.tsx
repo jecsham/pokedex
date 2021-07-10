6ba97161d29eea12d94ad84f25c4c480dba37ee5
jecsham <jecsham27@gmail.com>
2021-07-10 18:17:58 -0500

    frontend: optimized state system in home screen

--- frontend/src/pages/home/Card.tsx
+++ frontend/src/pages/home/Card.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
+import { PartialPokemon } from "../../adapters/http-pokemon";
 
-function Card(props: any) {
+function Card(props: PartialPokemon) {
   return (
     <div className="card shadow-sm">
       <img src={props.picture} draggable={false} className="card-img-top bg-secondary bg-gradient" alt="..." />
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,35 +1,47 @@
 import { useEffect, useState } from "react";
 import { fetchPokemons, Pokemon } from "../../adapters/http-pokemon";
+import ErrorAlert from "../../components/Erroralert";
 import Card from "./Card";
 
+interface ComponentState {
+  items: Pokemon[];
+  isLoading: boolean;
+  error: string | null;
+}
+
 function Home() {
-  const [items, setItems] = useState<Pokemon[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [state, setState] = useState<ComponentState>({
+    items: [],
+    isLoading: true,
+    error: null,
+  });
 
   const loadPokemons = async () => {
-    let map = await fetchPokemons();
-    if (map) {
-      setItems(
-        Array.from(map, ([n, v]) => ({
-          name: n,
-          id: v?.id,
-          picture: v?.picture,
-          types: v?.types,
-        }))
-      );
-    }
-    setIsLoading(false);
+    let response = await fetchPokemons();
+    if (!response.success)
+      return setState({ ...state, error: response.message });
+    setState({
+      ...state,
+      items: Array.from(response.result, ([n, v]) => ({
+        name: n,
+        id: v?.id,
+        picture: v?.picture,
+        types: v?.types,
+      })),
+      isLoading: false,
+      error: null,
+    });
   };
 
   const renderPlaceholder = () => {
-    if (isLoading) {
+    if (state.isLoading) {
       return (
         <div className="spinner-border" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
       );
     } else {
-      return items.map((item) => (
+      return state.items.map((item) => (
         <div key={item.id} className="col-12 col-sm-6 col-lg-4 g-3">
           <Card name={item.name} picture={item.picture} />
         </div>
